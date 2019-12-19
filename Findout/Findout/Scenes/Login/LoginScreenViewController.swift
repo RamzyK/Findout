839bdc161f41b4b78733b8f40e317bb3979ce994
RamzyK <gkermad.ramzy@gmail.com>
2019-12-19 13:10:24 +0100

    Init views

---
+++ Findout/Findout/Scenes/Login/LoginScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginScreenViewController.swift
+//  Findout
+//
+//  Created by Ramzy Kermad on 19/12/2019.
+//  Copyright © 2019 Ramzy Kermad. All rights reserved.
+//
+
+import UIKit
+
+class LoginScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
