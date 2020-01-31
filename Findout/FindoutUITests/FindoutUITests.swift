a15dce60a1e0a2fc2b44d1aac52ce07358628e7a
RamzyK <gkermad.ramzy@gmail.com>
2020-01-31 19:40:23 +0100

    Init commit for Fastlane features (Screenshots, Twitter, etc)

---
+++ Findout/FindoutUITests/FindoutUITests.swift
@@ -0,0 +1,90 @@
+//
+//  FindoutUITests.swift
+//  FindoutUITests
+//
+//  Created by Ramzy Kermad on 27/01/2020.
+//  Copyright © 2020 Ramzy Kermad. All rights reserved.
+//
+
+import XCTest
+
+class FindoutUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func localizedString(key:String) -> String {
+          let languageBundlePath = Bundle(for: FindoutUITests.self).path(forResource: deviceLanguage, ofType: "lproj") ?? Bundle(for: FindoutUITests.self).path(forResource: NSLocale.current.languageCode!, ofType: "lproj")
+          let localizationBundle = Bundle(path: languageBundlePath!)
+          let result = NSLocalizedString(key, bundle:localizationBundle!, comment: "")
+        return result
+    }
+
+    func testExample() {
+        // UI tests must launch the application that they test.
+        //let localizationBundle = localizedString(key: "signup.signupButtonLabel")
+
+        snapshot("Login Screen")
+        let app = XCUIApplication()
+
+        
+        app.buttons[localizedString(key: "login.signupButtonLabel")].tap()
+        snapshot("Sign up Screen")
+        
+        app/*@START_MENU_TOKEN@*/.buttons["✕"]/*[[".scrollViews.buttons[\"✕\"]",".buttons[\"✕\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.staticTexts["✕"].tap()
+        
+        let element = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+        
+        let textField = element.children(matching: .textField).element
+        textField.tap()
+        textField.tap()
+        textField.typeText("rmz@gmail.com")
+        
+        let secureTextField = element.children(matching: .secureTextField).element
+        
+        secureTextField.tap()
+        secureTextField.tap()
+        secureTextField.typeText("azertyuiop")
+        
+        app.buttons[localizedString(key: "login.loginButtonLabel")].tap()
+        
+        
+        let collectionViewsQuery = app.collectionViews
+        sleep(5)
+        snapshot("Categories screen")
+        collectionViewsQuery.children(matching: .cell).element(boundBy: 2).children(matching: .other).element.children(matching: .other).element.tap()
+        snapshot("Categories screen")
+        collectionViewsQuery.cells.children(matching: .other).element.children(matching: .other).element.tap()
+        snapshot("Map Screen")
+//        app.alerts["Autoriser « Findout » à accéder à votre position ?"].scrollViews.otherElements.buttons["Autoriser lorsque l’app est active"].tap()
+//        app.otherElements["KELBAY"].tap()
+//
+//        snapshot("Map Screen with bottom - sheet")
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    
+    
+    func testLaunchPerformance() {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
---
+++ Findout/fastlane/README.md
@@ -0,0 +1,44 @@
+fastlane documentation
+================
+# Installation
+
+Make sure you have the latest version of the Xcode command line tools installed:
+
+```
+xcode-select --install
+```
+
+Install _fastlane_ using
+```
+[sudo] gem install fastlane -NV
+```
+or alternatively using `brew cask install fastlane`
+
+# Available Actions
+## iOS
+### ios first_lane
+```
+fastlane ios first_lane
+```
+Description of what the lane does
+### ios screenshot
+```
+fastlane ios screenshot
+```
+Take screenshots
+### ios tweet
+```
+fastlane ios tweet
+```
+Creates a tweet on new version
+### ios do_testings
+```
+fastlane ios do_testings
+```
+Run test on Findout
+
+----
+
+This README.md is auto-generated and will be re-generated every time [fastlane](https://fastlane.tools) is run.
+More information about fastlane can be found on [fastlane.tools](https://fastlane.tools).
+The documentation of fastlane can be found on [docs.fastlane.tools](https://docs.fastlane.tools).
