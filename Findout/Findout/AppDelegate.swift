82bd12a09d74fb662dd89a5babc7a826b4840435
Nassim <nassim.morouche1@gmail.com>
2020-02-10 16:48:30 +0100

    quickFixes

--- Findout/Findout/AppDelegate.swift
+++ Findout/Findout/AppDelegate.swift
@@ -26,19 +26,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Customize the UI of GMSAutocompleteViewController
         // Set some colors (colorLiteral is convenient)
-          let barColor: UIColor =  _ColorLiteralType(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
-        let backgroundColor: UIColor =  _ColorLiteralType(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-        let textColor: UIColor =  _ColorLiteralType(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        let barColor: UIColor = #colorLiteral(red: 0.2193810642, green: 0.7583789825, blue: 0.4023743272, alpha: 1)
+        let backgroundColor: UIColor =  #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        let textColor: UIColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+
         // Navigation bar background.
-          UINavigationBar.appearance().barTintColor = barColor
-          UINavigationBar.appearance().tintColor = UIColor.white
+        UINavigationBar.appearance().barTintColor = barColor
+        UINavigationBar.appearance().tintColor = UIColor.white
         // Color and font of typed text in the search bar.
-          let searchBarTextAttributes = [NSAttributedString.Key.foregroundColor: textColor, NSAttributedString.Key.font: UIFont(name: "Helvetica Neue", size: 16)]
+        let searchBarTextAttributes = [NSAttributedString.Key.foregroundColor: textColor, NSAttributedString.Key.font: UIFont(name: "Helvetica Neue", size: 16)]
         UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).defaultTextAttributes = searchBarTextAttributes as [NSAttributedString.Key : Any]
         // Color of the placeholder text in the search bar prior to text entry
-          let placeholderAttributes = [NSAttributedString.Key.foregroundColor: backgroundColor, NSAttributedString.Key.font: UIFont(name: "Helvetica", size: 15)]
+        let placeholderAttributes = [NSAttributedString.Key.foregroundColor: backgroundColor, NSAttributedString.Key.font: UIFont(name: "Helvetica", size: 15)]
         // Color of the default search text.
-          let attributedPlaceholder = NSAttributedString(string: "Search", attributes: placeholderAttributes as [NSAttributedString.Key : Any])
+        let attributedPlaceholder = NSAttributedString(string: "Search", attributes: placeholderAttributes as [NSAttributedString.Key : Any])
         UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).attributedPlaceholder = attributedPlaceholder
         
         return true
